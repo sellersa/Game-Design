6631723569af010b23e6cdb25a34271a7f06203e
sellersa <143823910+sellersa@users.noreply.github.com>
2024-01-24 13:36:31 -0600

    Initial commit

---
+++ HelloWorld.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_btn_show_pressed():
+	$Label.text = "Minecraft Club"
+
+
+func _on_btn_clear_pressed():
+	$Label.text = ""
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
