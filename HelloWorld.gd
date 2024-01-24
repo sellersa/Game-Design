7a3e59bd8cbf76a6c41dc44008ef5692fae4b9e5
sellersa <143823910+sellersa@users.noreply.github.com>
2024-01-24 13:43:58 -0600

    Update HelloWorld.gd

--- HelloWorld.gd
+++ HelloWorld.gd
@@ -3,13 +3,12 @@ extends Control
 
 
 
+
 func _on_btn_show_pressed():
 	$Label.text = "Minecraft Club"
 
-
 func _on_btn_clear_pressed():
 	$Label.text = ""
-
 
 func _on_btn_exit_pressed():
 	get_tree().quit()
