ea5a5000151640ca05cabf416ab0ded780c03942
sellersa <143823910+sellersa@users.noreply.github.com>
2024-02-22 13:43:09 -0600

    2/22

---
+++ plt_world.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+
+func _ready():
+	$world/CollisionPolygon2D/Polygon2D.polygon = $world/CollisionPolygon2D.polygon
+
+
+func _on_interact_mouse_entered():
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		$interact.queue_free()
+
+
+var dragging = false
+var offset = Vector2.ZERO
+
+
+
+func _process(delta):
+	if dragging:
+		$draggable.global_position = get_global_mouse_position() - offset
+
+func _on_button_button_down():
+	dragging = true
+	offset = get_global_mouse_position() - $draggable.global_position
+
+func _on_button_button_up():
+	dragging = false
