14a4d25c700b7449742efad227bae5757732818f
sellersa <143823910+sellersa@users.noreply.github.com>
2024-03-01 13:42:25 -0600

    3/1

---
+++ phys_demo.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+func _ready():
+	$Panel/GravSlide.value = $phys_ball.gravity_scale
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var nball = preload("res://phys_ball.tscn").instantiate()
+		nball.position = event.position
+		add_child(nball)
+
+
+func _on_grav_slide_value_changed(value):
+	for child in get_children():
+		if child is RigidBody2D:
+			child.gravity_scale = value
+
+
+func _on_button_pressed():
+	for child in get_children():
+		if child is RigidBody2D:
+			var force = randi_range(50, 1000)
+			child.apply_central_impulse(Vector2(1,0) * 500)
