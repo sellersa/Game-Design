7917a1473bd4dd315a90a098a9220e29d9e1ece6
sellersa <143823910+sellersa@users.noreply.github.com>
2024-03-26 13:42:40 -0500

    3/26

--- entities/mini_heart.gd
+++ entities/mini_heart.gd
@@ -1,8 +1,8 @@
 extends BaseItem
 
-func _init(): super._init(5)
+func _init(): super._init(1)
 func _ready(): super._ready()
 
 func interact(player):
-	player.pickup_health(value)
+	player.pickup_money(value)
 	remove()
