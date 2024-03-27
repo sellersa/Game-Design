b1d57644adaac1f3e07eb85d4e19f995002e0591
sellersa <143823910+sellersa@users.noreply.github.com>
2024-03-27 13:47:38 -0500

    3/27

--- entities/mini_heart.gd
+++ entities/mini_heart.gd
@@ -1,8 +1,8 @@
 extends BaseItem
 
-func _init(): super._init(1)
+func _init(): super._init(5)
 func _ready(): super._ready()
 
 func interact(player):
-	player.pickup_money(value)
+	player.pickup_health(value)
 	remove()
--- td_player.gd
+++ td_player.gd
@@ -1,37 +1,95 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-var inertia = Vector2()
-var look_direction = Vector2.DOWN
+const SPEED = 100.0
+const MAXIMUM_OBTAINABLE_HEALTH = 400.0
 
+enum STATES { IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING }
+
+@export var data = {
+	"max_health": 60.0,  # 20hp per heart; 5 per fraction
+	"health": 60.0,      # Min 60 Max 400
+	"money": 0,
+	"state": STATES.IDLE,
+	"secondaries": [],
+}
+
+var inertia = Vector2()
+var look_direction = Vector2.DOWN  # (0, 1)
+var animation_lock = 0.0
+var damage_lock = 0.0
+var charge_time = 2.5
+var charge_start_time = 0.0
+
+var slash_scene = preload("res://entities/attacks/slash.tscn")
 var menu_scene = preload("res://my_gui.tscn")
 var menu_instance = null
 
+@onready var p_HUD = get_tree().get_first_node_in_group("HUD")
+
+func get_direction_name():
+	return ["right", "down", "left", "up"][
+		int(round(look_direction.angle() * 2 / PI)) % 4
+	]
+
+func attack():
+	data.state = STATES.ATTACKING
+	var dir_name = get_direction_name()
+	if dir_name == "left":
+		$AnimatedSprite2D.flip_h = 0
+	$AnimatedSprite2D.play("swipe_" + dir_name)
+	var attack_direction = look_direction
+	var slash = slash_scene.instantiate()
+	slash.position = attack_direction * 20.0
+	slash.rotation = Vector2().angle_to_point(-attack_direction)
+	add_child(slash)
+	animation_lock = 0.2
+
+
+func pickup_money(value):
+	data.money += value
+
+func pickup_health(value):
+	data.health += value
+	data.health = clamp(data.health, 0, data.max_health)
+
 func _ready():
+	p_HUD.show()
 	menu_instance = menu_scene.instantiate()
 	$Camera2D.add_child.call_deferred(menu_instance)
 	menu_instance.hide()
 
 func _physics_process(delta):
-	var direction = Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	)
-	update_animation(direction)
-	if direction.length() > 0:
-		look_direction = direction
-		# Scale to 1 to prevent diagonal boosts
-		direction = direction.normalized()
-		velocity = direction * SPEED
-	else:
-		velocity = velocity.move_toward(Vector2(), SPEED)
-	velocity += inertia
-	move_and_slide()
-	inertia = inertia.move_toward(Vector2(), delta * 1000.0)
-
+	animation_lock = max(animation_lock-delta, 0.0)
+	damage_lock = max(damage_lock-delta, 0.0)
+	
+	if animation_lock == 0.0 and data.state != STATES.DEAD:
+		if data.state != STATES.CHARGING:
+			data.state - STATES.IDLE
+			
+		var direction = Vector2(
+			Input.get_axis("ui_left", "ui_right"),
+			Input.get_axis("ui_up", "ui_down")
+		)
+		update_animation(direction)
+		if direction.length() > 0:
+			look_direction = direction
+			# Scale to 1 to prevent speed boost from diagonals
+			direction = direction.normalized()
+			velocity = direction * SPEED
+		else:
+			velocity = velocity.move_toward(Vector2(), SPEED)
+		velocity += inertia
+		move_and_slide()
+		inertia = inertia.move_toward(Vector2(), delta * 1000.0)
+	
+	if data.state != STATES.DEAD:
+		if Input.is_action_just_pressed("ui_accept"):
+			attack()
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
 		get_tree().paused = true
+
 
 func update_animation(direction):
 	var a_name = "idle_"
